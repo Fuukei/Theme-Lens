c1348ca558a622276902a79957cd2bdfdf204f39
染川 瞳 <me@hareru.org>
2024-10-27 15:31:58 +0900

    Navbar

--- packages/theme-lens/src/navbar/Navbar.vue
+++ packages/theme-lens/src/navbar/Navbar.vue
@@ -32,9 +32,10 @@ const props = defineProps<{
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background: rgba(255, 255, 255, 0.85);
+  background: rgba(255, 255, 255, 0.75);
   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
   z-index: 1000;
+  backdrop-filter: saturate(180%) blur(10px);
 
   &.active-header {
     backdrop-filter: saturate(180%) blur(10px);
@@ -50,6 +51,8 @@ const props = defineProps<{
     transform: translateX(-50%);
     width: fit-content;
     border-radius: 50px;
+    border: 1px solid #f7f7f7;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
   }
 
   .navbar-title {
@@ -62,7 +65,7 @@ const props = defineProps<{
   .navbar-links {
     ul {
       display: flex;
-      gap: 0.55rem;
+      gap: 0.1875rem;
 
       li {
         list-style: none;
@@ -71,17 +74,20 @@ const props = defineProps<{
           text-decoration: none;
           font-size: 1rem;
           font-weight: 500;
-          color: #333; 
+          color: rgba(0, 0, 0, 0.6); 
           position: relative;
-          transition: color 0.3s, background-color 0.3s;
-          padding: 0.375rem max(0.75rem, env(safe-area-inset-left)) 0.375rem max(0.75rem, env(safe-area-inset-right)); /* Default padding */
+          transition: color 0.3s, background-color 0.3s, text-shadow 0.3s;
+          padding: 0.375rem max(1rem, env(safe-area-inset-left)) 0.375rem max(1rem, env(safe-area-inset-right)); 
           border-radius: 50px; 
+          text-shadow: 1px 1px 5px rgba(85, 85, 85, 0.175); 
 
-          &:hover {
+            &:hover {
             color: #007bff; 
             background-color: rgba(0, 123, 255, 0.1); 
             border-radius: 50px;
-          }
+            text-shadow: 1px 1px 5px rgba(0, 123, 255, 0.175); 
+            box-shadow: inset 0 0 10px rgba(255, 255, 255, 0.2); 
+            }
         }
       }
     }
