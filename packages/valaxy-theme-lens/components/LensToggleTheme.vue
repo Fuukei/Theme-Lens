5e571e51a3ecadbad75805e08cf6acd407edd565
WRXinYue <wrxinyue@proton.me>
2024-10-27 21:24:51 +0800

    chore: update LensToggleTheme class name

--- packages/valaxy-theme-lens/components/LensToggleTheme.vue
+++ packages/valaxy-theme-lens/components/LensToggleTheme.vue
@@ -5,7 +5,7 @@ const appStore = useAppStore()
 </script>
 
 <template>
-  <button class="sakura-icon-btn sakura-toggle-theme" type="button" aria-label="Toggle Dark Mode" @click="appStore.toggleDarkWithTransition">
+  <button class="lens-toggle-theme" type="button" aria-label="Toggle Dark Mode" @click="appStore.toggleDarkWithTransition">
     <div :class="appStore.isDark ? 'i-ri-moon-line' : 'i-ri-sun-line'" />
   </button>
 </template>
